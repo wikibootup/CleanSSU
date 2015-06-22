f3ecf84e59c03a1f69f426b5df6e51613582fc88
flashgugu <flashilver@gmail.com>
2015-06-22 17:40:56 +0900

    POST로 요청을 받아서 각각의 데이터들을 DB에 입력하는것까지 개발

--- app/__init__.py
+++ app/__init__.py
@@ -7,14 +7,3 @@ db = SQLAlchemy(app)
 
 #Fix location This code
 from app import models, controllers
-
-# u = models.PaymentInfo(admin_group='dongyeon', card_company='sinhan', date_time='20150622', cost='10000', payment_store='Club')
-# db.session.add(u)
-# db.session.commit()
-
-# print("Success!")
-
-# paymentinfos = models.PaymentInfo.query.all()
-
-# for item in paymentinfos:
-# 	print(item.id, item.admin_group)
--- app/controllers.py
+++ app/controllers.py
@@ -1,5 +1,8 @@
 from flask import request
-from app import app
+from app import app, models
+from flask.ext.sqlalchemy import SQLAlchemy 
+
+db = SQLAlchemy(app)
 
 #Api write
 @app.route('/insertdata', methods=['GET', 'POST'])
@@ -12,7 +15,11 @@ def insertdata():
 		cost = request.form['cost']
 		payment_store = request.form['payment_store']
 
-		return payment_store
+		paymentinfo = models.PaymentInfo(admin_group=admin_group, card_company=card_company, date_time=date_time, cost=cost, payment_store=payment_store)
+		db.session.add(paymentinfo)
+		db.session.commit()
+
+		return "OK!"
 
 	if request.method=='GET':
 		return "Hello, World!"
