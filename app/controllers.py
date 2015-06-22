a079b1c88fc4ee2f5622969d5ec94bc06b05342c
flashgugu <flashilver@gmail.com>
2015-06-22 17:29:36 +0900

    gitignore에 pyc추가, GET, POST작동 확인

--- app/controllers.py
+++ app/controllers.py
@@ -1,3 +1,19 @@
-from flask import request, flash, g, session, redirect, url_for
-from app import db
-from models import PaymentInfo
+from flask import request
+from app import app
+
+#Api write
+@app.route('/insertdata', methods=['GET', 'POST'])
+def insertdata():
+
+	if request.method=='POST':
+		admin_group = request.form['admin_group']
+		card_company = request.form['card_company']
+		date_time = request.form['date_time']
+		cost = request.form['cost']
+		payment_store = request.form['payment_store']
+
+		return payment_store
+
+	if request.method=='GET':
+		return "Hello, World!"
+
