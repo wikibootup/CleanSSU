45ce7e6eef3e9481daa2c1d2c6866520767b28e6
flashgugu <flashilver@gmail.com>
2015-06-22 16:34:12 +0900

    데이터베이스에 값이 입력되는것을 확인

--- app/__init__.py
+++ app/__init__.py
@@ -6,4 +6,15 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 
 #Fix location This code
-from app import api, model, database
+from app import models, controllers
+
+# u = models.PaymentInfo(admin_group='dongyeon', card_company='sinhan', date_time='20150622', cost='10000', payment_store='Club')
+# db.session.add(u)
+# db.session.commit()
+
+# print("Success!")
+
+# paymentinfos = models.PaymentInfo.query.all()
+
+# for item in paymentinfos:
+# 	print(item.id, item.admin_group)
--- app/models.py
+++ app/models.py
@@ -2,19 +2,19 @@ from app import db
 
 class PaymentInfo(db.Model):
 
-	__tablename__ = 'payment_info'
-
-	number = db.Column(db.Integer, primary_key=True, nullable=False)
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True,nullable=False)
 	admin_group = db.Column(db.String(128), nullable=False)
 	card_company = db.Column(db.String(128), nullable=False)
-	date_time = db.Column(db.DateTime, nullable=False)
+	date_time = db.Column(db.String(128), nullable=False)
 	cost = db.Column(db.Integer(), nullable=False)
 	payment_store = db.Column(db.String(128), nullable=False)
 
-	def __init__(self, number, admin_group, card_company, date_time, cost, payment_store):
-		self.number = number
+	def __init__(self, admin_group, card_company, date_time, cost, payment_store):
 		self.admin_group = admin_group
 		self.card_company = card_company
 		self.date_time = date_time
 		self.cost = cost
 		self.payment_store = payment_store
+
+	def __repr__(self):
+		return '<PaymentInfo %r>' % (self.payment_store)
